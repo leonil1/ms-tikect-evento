bfa527770fb85ca4200ce2d11345d1b080238cd4
devleonil <chipana.leo1@gmail.com>
2024-03-06 10:25:12 -0500

    estructura de ms-eventos

---
+++ src/main/java/com/gruopo9/msevento/MsEventoApplication.java
@@ -0,0 +1,13 @@
+package com.gruopo9.msevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruopo9/msevento/MsEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruopo9.msevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
